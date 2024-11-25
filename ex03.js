6d1f44d40ba78ca1eda500ad4aad00f0f7c2629e
BiscuitCuit15 <ldescombes@edenschool.fr>
2024-11-25 14:45:12 +0100

    feat (app): add ex03.js

---
+++ ex03.js
@@ -0,0 +1,14 @@
+// ex03.js
+
+function multiplyArray(arr, multiplier) {
+    let array = [];
+	for (let i = 0; i < arr.length; i++) {
+        let result = arr[i] * multiplier;
+        array.push(result)
+    }
+    return array;
+}
+
+
+console.log(multiplyArray([1, 2, 3], 2)); // Résultat attendu : [2, 4, 6]
+console.log(multiplyArray([4, 0, -3], 3)); // Résultat attendu : [12, 0, -9]
